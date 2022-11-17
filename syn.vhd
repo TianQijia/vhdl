7f4a77a987146e220fd3a217a4bbdd1d69a78136
TianQijia <78639124+TianQijia@users.noreply.github.com>
2022-11-17 22:36:17 +0800

    Update syn.vhd

---
+++ syn.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity syn is
+    PORT (
+        S: in std_logic;
+        F: out std_logic;
+        clk: in std_logic
+    );
+end syn;
+
+architecture rtl of syn is
+    signal potential: unsigned(7 downto 0) := "00000000";
+begin
+    F <= '0';
+    process(clk)
+    begin
+        if (rising_edge(clk)) then
+            if ( s = '1') then
+                potential <= potential + 100;
+            else
+                potential <= potential / 2;
+            end if;
+            if ( potential > 160 ) then
+                F <= '1';
+            else
+                F <= '0';
+            end if;
+        end if;
+    end process;
+end rtl;
